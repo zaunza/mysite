f4ec2c7c3a2c131044ca0cdf77223b861fb46d0c
zaunza <1620279+zaunza@users.noreply.github.com>
2020-09-09 22:22:21 -0700

    Updates

---
+++ precache-manifest.faebe9859b8edeff30cb8f6c721ff249.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fde6fa2e66c200f56faff602630148e4",
+    "url": "/mysite/index.html"
+  },
+  {
+    "revision": "97346f66637284f2a28d",
+    "url": "/mysite/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "30c575e627ebae51e775",
+    "url": "/mysite/static/js/2.52cf36a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mysite/static/js/2.52cf36a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97346f66637284f2a28d",
+    "url": "/mysite/static/js/main.bdee7e5a.chunk.js"
+  },
+  {
+    "revision": "1d99fe0da908ca569518",
+    "url": "/mysite/static/js/runtime-main.1d89857a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mysite/static/media/logo.5d5d9eef.svg"
+  }
+]);
